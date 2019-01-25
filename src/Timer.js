5b928ce7ad07a7ed2af56494bb437ab6bbcc67e7
Peter Kreppein <pmkreppein@gmail.com>
2019-01-25 20:51:03 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,9 @@ class Timer extends Component {
     }
   }
 
-
+componentDidUpdate() {
+    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+  }
 
   //Your code here
 
